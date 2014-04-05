e08e539a22351c2e1bb10ffec30766b4b6211842
Enrico Scholz <enrico.scholz@ensc.de>
2014-04-05 17:31:50 +0200

    ARM: implemented TLS startup code
    doing it in the stackgap initialization is hacky but done in upstream
    code so we do it for ARM too.
    
    Signed-off-by: Enrico Scholz <enrico.scholz@ensc.de>

--- lib/stackgap-common.h
+++ lib/stackgap-common.h
@@ -148,7 +148,6 @@ void __setup_tls(tcbhead_t* mainthread) {
 #elif defined(__aarch64__)
   asm volatile ("msr tpidr_el0, %0" :: "r"(mainthread));
 #elif defined(__arm__)
-  extern void __arm_set_tls(void*);
   __arm_set_tls(mainthread);
 #elif defined(__hppa__)
   /* control register 27 is used as thread pointer on PA-RISC Linux,
